df4bdd3c3ec8393ad05b599a7e798cbfd599bb5b
Andrii Kirmas <kirmas@phystech.edu>
2021-02-22 09:03:19 +0300

    Start v8 boilerplate

---
+++ postcss-d-ts-8/index.js
@@ -0,0 +1,28 @@
+module.exports = (opts = { }) => {
+
+  // Work with options here
+
+  return {
+    postcssPlugin: 'postcss-d-ts',
+    /*
+    Root (root, postcss) {
+      // Transform CSS AST here
+    }
+    */
+
+    /*
+    Declaration (decl, postcss) {
+      // The faster way to find Declaration node
+    }
+    */
+
+    /*
+    Declaration: {
+      color: (decl, postcss) {
+        // The fastest way find Declaration node if you know property name
+      }
+    }
+    */
+  }
+}
+module.exports.postcss = true
