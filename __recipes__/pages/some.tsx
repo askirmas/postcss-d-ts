6b59a170f42f14ff69f99d2926fe08acc8bf2fe3
Andrii Kirmas <kirmas@phystech.edu>
2021-03-15 16:03:16 +0300

    Add gifs

--- __recipes__/pages/some.tsx
+++ __recipes__/pages/some.tsx
@@ -1,7 +1,11 @@
+import css from "../styles/some.css"
+import stylus from "../styles/some.styl"
+import less from "../styles/some.less"
+import scss from "../styles/some.scss"
 
-export default function Page() {
+export default function Some() {
   return <main>
-    <div className={css.dialog__description}>css</div>
+    <div className={css.dialog__title}>css</div>
     <div className={stylus.dialog__close}>stylus</div> 
     <div className={less.dialog__cancel}>less</div>
     <div className={scss.dialog__submit}>scss</div>   
