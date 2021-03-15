0ba7e61672f459330e8e3ba4609b24791972bb89
Andrii Kirmas <kirmas@phystech.edu>
2021-03-15 12:57:21 +0300

    Prepare for gif

--- __recipes__/pages/some.tsx
+++ __recipes__/pages/some.tsx
@@ -1,13 +1,9 @@
-import css from "../styles/some.css"
-import scss from "../styles/some.scss"
-import less from "../styles/some.less"
-import stylus from "../styles/some.styl"
 
 export default function Page() {
   return <main>
-    <div className={css.dialog__title}>css</div>
-    <div className={scss.dialog__submit}>scss</div>    
+    <div className={css.dialog__description}>css</div>
+    <div className={stylus.dialog__close}>stylus</div> 
     <div className={less.dialog__cancel}>less</div>
-    <div className={stylus.dialog__close}>stylus</div>
+    <div className={scss.dialog__submit}>scss</div>   
   </main>
 }
