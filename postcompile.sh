73def0a069d2481b618a45db1e10e2f5375c8e46
Andrii Kirmas <kirmas@phystech.edu>
2021-03-16 19:27:19 +0300

    Purify dependencies by using `pack`

--- postcompile.sh
+++ postcompile.sh
@@ -3,7 +3,7 @@
 # From https://github.com/askirmas/react-classnaming/blob/main/postcompile.sh
 
 src="src"
-types="$npm_package_types" 
+types="$npm_package_types"
 find "$src" -name '*.d.ts' -exec cp {} "$types" \;
 
 dist="$(dirname "$npm_package_main")"
@@ -11,3 +11,5 @@ dist="$(dirname "$npm_package_main")"
 find "$dist" -name "*.types.js" -delete
 
 find "$dist" -name "*.d.ts" -not -name "options.*" -not -name "ts-swiss.*" -not -name "_css-template.*" -delete
+
+mv "$(npm pack --quiet | tail -n 1)" pack.tgz
