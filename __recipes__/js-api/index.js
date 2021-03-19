50b922c1a7d9052da0609f9aa5e50a82e8cdec94
Andrii Kirmas <kirmas@phystech.edu>
2021-03-19 12:35:36 +0300

    #21 Show appropriate usage

--- __recipes__/js-api/index.js
+++ __recipes__/js-api/index.js
@@ -3,18 +3,11 @@ const postcss = require('postcss')
 const fs = require('fs')
 
 fs.readFile('index.css', (_, css) => {
-  try {
-    postcss([
-      dts(),
-    ], { from: 'index.css', to: 'out.css' })
-      .process(css)
-      .then(result => {
-        console.log(result.css);
-        process.exit(1);
-      })
-  } catch(e) {
-    process.exit(0)
-  } finally {
-    process.exit(1)
-  }
+  postcss([
+    dts(),
+  ])
+  .process(css, { from: 'index.css', to: 'out.css' })
+  .then(result => {
+    console.log(result.css);
+  })
 })
